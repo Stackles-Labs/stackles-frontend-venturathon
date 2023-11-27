b33597125f7ea02c29c33e6851886bf8a57786e1
Ayush Singh <ayushsinghmi711@gmail.com>
2023-11-27 12:04:49 +0530

    links added

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
-import { FaLinkedin ,FaDiscord,FaTwitter} from "react-icons/fa";
+import { FaLinkedin ,FaDiscord,FaTwitter, FaGithub} from "react-icons/fa";
+import { NavLink } from 'react-router-dom';
 
 
 function Header() {
@@ -8,13 +9,31 @@ function Header() {
      <div className='text-4xl font-bold ml-[40px] text-purple-500'>Stackles</div>
      <div className='flex gap-10 text-2xl mr-[40px] '>
 
+     <NavLink
+     to="https://www.linkedin.com/company/stackles/"
+     target="_blank">
      <div>
      <FaLinkedin />
      </div>
+     </NavLink>
+   
+    <NavLink
+    to="https://github.com/RnDAO"
+    target="_blank">
+    <div>
+    <FaGithub />
+    </div>
+    </NavLink>
+     
+      <NavLink
+      to="https://twitter.com/StacklesApp"
+      target="_blank">
+      <div>
+      <FaTwitter />
+      </div>
+      </NavLink>
 
-     <div><FaTwitter /></div>
 
-     <div><FaDiscord /></div>
      </div>
     </div>
   )
